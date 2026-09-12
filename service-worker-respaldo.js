cb72379265eef48dd004fc1b2a77f7a03f80c82a
MULTIINVERSIONESJCP <jeissoncamilo.mar@gmail.com>
2026-09-12 14:35:43 -0500

    Delete service-worker-respaldo.js

--- service-worker-respaldo.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='multi-jcp-v80-work';
-const APP_SHELL=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png','/icon-maskable-192.png','/icon-maskable-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,cp)).catch(()=>{});return r;}).catch(()=>caches.match(e.request).then(c=>c||caches.match('/index.html'))));});
