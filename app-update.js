68379ec20ec7a904bf243bd9fe6ba5e102f964cf
MULTIINVERSIONESJCP <jeissoncamilo.mar@gmail.com>
2026-09-15 00:32:57 -0500

    Add files via upload

--- app-update.js
+++ app-update.js
@@ -1,7 +1,7 @@
 (()=>{
   'use strict';
 
-  const APP_VERSION='2026.09.15.1';
+  const APP_VERSION='2026.09.15.2';
   const CHECK_EVERY_MS=10*60*1000;
   let registrationRef=null;
   let banner=null;
