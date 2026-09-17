de027511ec03ea7ab78f914fa0ed1a3c0491135a
MULTIINVERSIONESJCP <jeissoncamilo.mar@gmail.com>
2026-09-17 18:53:48 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION='2026.09.17.4';
+const APP_VERSION='2026.09.17.5';
 const CACHE_NAME='multi-jcp-'+APP_VERSION;
 const APP_BASE=self.registration.scope;
 const INDEX_URL=new URL('index.html',APP_BASE).href;
